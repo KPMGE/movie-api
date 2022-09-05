c45929d64e347bac2a27830f2ff2939e875dafb2
KPMGE <jesuskevin254@gmail.com>
2022-09-05 12:17:14 -0300

    :sparkles: implement showMovies route with dummy data

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"movie.api.kpmge/internal/data"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -17,6 +20,18 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		http.NotFound(w, r)
 		return
 	}
+
+	movie := data.Movie{
+		ID:        100,
+		CreatedAt: time.Now(),
+		Title:     "the party is over",
+		Year:      2003,
+		Runtime:   200,
+		Genres:    []string{"comedy", "drama", "fiction", "romance"},
+		Version:   2302,
+	}
+
+	app.writeJSON(w, http.StatusOK, movie, nil)
 
 	// write message into the response
 	fmt.Fprintf(w, "show details of the movie: %d", id)
