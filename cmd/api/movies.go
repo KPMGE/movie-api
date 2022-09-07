14d606b20e0eb7e460e9be7e43489d9fab737eb4
KPMGE <jesuskevin254@gmail.com>
2022-09-07 17:31:25 -0300

    :recycle: create a badRequest handler helper

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -18,7 +18,7 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 
 	err := app.readJSON(w, r, &input)
 	if err != nil {
-		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		app.badRequestResponse(w, r, err)
 		return
 	}
 
