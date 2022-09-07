613787398e72171c10a042e105ab9be2e66d6716
KPMGE <jesuskevin254@gmail.com>
2022-09-07 17:28:49 -0300

    :sparkles: create a readJSON helper for dealing with json errors

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
 	"net/http"
 	"strconv"
 
@@ -25,7 +27,50 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	return id, nil
 }
 
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any) error {
+	// decode json body into the target destination
+	err := json.NewDecoder(r.Body).Decode(dst)
+	if err != nil {
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+
+		switch {
+		// if the error has a syntaxError
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("body contains badly formed JSON (at character %d)", syntaxError.Offset)
+
+		// if the error is a ErrUnexpectedEOF
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly formed JSON")
+
+		// if the error has a unmarshalTypeError
+		case errors.As(err, &unmarshalTypeError):
+			if unmarshalTypeError.Field != "" {
+				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
+			}
+			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
+
+		// if the body is empty
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+
+		case errors.As(err, &invalidUnmarshalError):
+			// it's fine to panic here, cuz if that error occured, probably it's an error from the developer
+			// so, it makes sense to catch it and stop the application as soon as possible
+			panic(err)
+
+		default:
+			return err
+		}
+	}
+	return nil
+}
+
+// converts entry in json and write it to the response
 func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+	// this works exactly as the Marshal function, but it indents the json,
+	// which is good when using curl.
 	js, err := json.MarshalIndent(data, "", "  ")
 
 	if err != nil {
--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -17,7 +16,7 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 		Genres  []string `json:"genres"`
 	}
 
-	err := json.NewDecoder(r.Body).Decode(&input)
+	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 		return
