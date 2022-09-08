539a2415e804bd9e7086f8566460f442e7eb6926
KPMGE <jesuskevin254@gmail.com>
2022-09-07 22:42:40 -0300

    :sparkles: accept custom Runtime at request body

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,11 +1,15 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 type Runtime int32
+
+var ErrInvalidRuntimeFormat = errors.New("Invalid Runtime format")
 
 // implementing MarshalJSON function for the type Runtime so that
 // when calling json.Marshal(), the return will be of the right format
@@ -16,4 +20,28 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	quotedJsonValue := strconv.Quote(jsonValue)
 
 	return []byte(quotedJsonValue), nil
+}
+
+// implementing UnmarshalJSON function, so that we can accept a custom Runtime
+// in the response body
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedValue, err := strconv.Unquote(string(jsonValue))
+
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquotedValue, " ")
+
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(i)
+	return nil
 }
