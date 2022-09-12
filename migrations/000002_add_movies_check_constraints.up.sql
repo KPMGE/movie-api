bec6174931ef13a08c9a0d1df87bc8ce048f71fc
KPMGE <jesuskevin254@gmail.com>
2022-09-12 20:49:39 -0300

    :recycle: fix typo on the migration

--- migrations/000002_add_movies_check_constraints.up.sql
+++ migrations/000002_add_movies_check_constraints.up.sql
@@ -1,3 +1,3 @@
 ALTER TABLE movies ADD CONSTRAINT movies_runtime_check CHECK (runtime >= 0);
-ALTER TABLE movies ADD CONSTRAINT movies_year_check CHECK (year BETWEEN 0 AND date_part('yearn', now()));
+ALTER TABLE movies ADD CONSTRAINT movies_year_check CHECK (year BETWEEN 0 AND date_part('year', now()));
 ALTER TABLE movies ADD CONSTRAINT genres_length_check CHECK (array_length(genres, 1) BETWEEN 1 AND 10);
